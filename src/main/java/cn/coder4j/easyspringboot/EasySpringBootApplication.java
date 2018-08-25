c18ded26fc57b85a2120f5ff4cea4041f67ae90f
buhao <buh@fshows.com>
2018-08-25 21:35:34 +0800

    项目初始化

---
+++ src/main/java/cn/coder4j/easyspringboot/EasySpringBootApplication.java
@@ -0,0 +1,12 @@
+package cn.coder4j.easyspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasySpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EasySpringBootApplication.class, args);
+    }
+}
---
+++ src/test/java/cn/coder4j/easyspringboot/EasySpringBootApplicationTests.java
@@ -0,0 +1,16 @@
+package cn.coder4j.easyspringboot;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EasySpringBootApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
